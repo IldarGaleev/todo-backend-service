5af23532da6ab9de67a5e8a9f33638c0254b27c4
Ильдар Галеев <ildar-galeev92@mail.ru>
2024-08-09 12:00:03 +0700

    feature: implement jwt secrets

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
@@ -19,7 +20,6 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/kataras/jwt v0.1.12 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	golang.org/x/crypto v0.26.0 // indirect
--- internal/lib/secrets/jwt.go
+++ internal/lib/secrets/jwt.go
@@ -2,39 +2,138 @@ package secretsJwt
 
 import (
 	"context"
+	"errors"
 	"log/slog"
 	"time"
 
 	configApp "github.com/IldarGaleev/todo-backend-service/internal/app/config"
 	secretsDTO "github.com/IldarGaleev/todo-backend-service/internal/lib/secrets/models"
-	"github.com/kataras/jwt"
+	"github.com/golang-jwt/jwt/v5"
 )
 
+var (
+	ErrVerifyError = errors.New("jwt verify error")
+	ErrCreateError = errors.New("jwt create error")
+)
+
+type IJWTIndexer interface {
+	CreateNewId(ctx context.Context) uint64
+}
+
+type IJWTRevoker interface {
+	IsJWTRevoked(ctx context.Context, id uint64) bool
+	RevokeJWT(ctx context.Context, id uint64)
+}
+
 type SecretJWT struct {
-	logger *slog.Logger
-	maxAge time.Duration
-	secretKey []byte
+	logger     *slog.Logger
+	maxAge     time.Duration
+	secretKey  []byte
+	jwtIndexer IJWTIndexer
+	jwtRevoker IJWTRevoker
 }
 
-func New(log *slog.Logger, config configApp.AppConfig) (*SecretJWT){
+type TokenClaims struct {
+	UserId   uint64 `json:"userid"`
+	Username string `json:"username"`
+	TokenId  uint64 `json:"tokenid"`
+	jwt.RegisteredClaims
+}
+
+func New(
+	log *slog.Logger,
+	config configApp.AppConfig,
+	jwtIndexer IJWTIndexer,
+	jwtRevoker IJWTRevoker,
+) *SecretJWT {
 	return &SecretJWT{
-		logger: log.With(slog.String("module","secretsJwt")),
-		maxAge: config.SecretsMaxAge,
-		secretKey: config.SecretKey,
+		logger:     log.With(slog.String("module", "secretsJwt")),
+		maxAge:     config.SecretsMaxAge,
+		secretKey:  config.SecretKey,
+		jwtIndexer: jwtIndexer,
+		jwtRevoker: jwtRevoker,
 	}
 }
 
-func (s *SecretJWT)ValidateSecret(ctx context.Context, secret []byte) (error){
-	// log:=s.logger.With(slog.String("method","ValidateSecret"))
-	panic("not implement ValidateSecret")
-}
+func (s *SecretJWT) decodeToken(secret []byte) (*TokenClaims, error) {
+	token, err := jwt.ParseWithClaims(
+		string(secret),
+		&TokenClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			return s.secretKey, nil
+		},
+	)
 
-func (s *SecretJWT)CreateSecret(ctx context.Context, user secretsDTO.User) ([]byte, error) {
-	log:=s.logger.With(slog.String("method","CreateSecret"))
-	token, err := jwt.Sign(jwt.HS256, s.secretKey, user.Payload, jwt.MaxAge(s.maxAge))
 	if err != nil {
-		log.Debug("jwt validation error",slog.Any("err",err))
-		return nil,err
+		return nil, err
 	}
-	return token, nil
+
+	if claims, ok := token.Claims.(*TokenClaims); ok {
+		return claims, nil
+	}
+
+	return nil, ErrVerifyError
+}
+
+func (s *SecretJWT) ValidateSecret(ctx context.Context, secret []byte) (*secretsDTO.User, error) {
+	log := s.logger.With(slog.String("method", "ValidateSecret"))
+
+	claims, err := s.decodeToken(secret)
+	if s.jwtRevoker.IsJWTRevoked(ctx, claims.TokenId) {
+		return nil, ErrVerifyError
+	}
+
+	if err != nil {
+		log.Debug("jwt parse error", slog.Any("err", err))
+		return nil, ErrVerifyError
+	}
+
+	return &secretsDTO.User{
+		UserId:   &claims.UserId,
+		Username: &claims.Username,
+	}, nil
+
+}
+
+func (s *SecretJWT) CreateSecret(ctx context.Context, user secretsDTO.User) ([]byte, error) {
+	log := s.logger.With(slog.String("method", "CreateSecret"))
+
+	claims := TokenClaims{
+		UserId:   *user.UserId,
+		Username: *user.Username,
+		TokenId:  s.jwtIndexer.CreateNewId(ctx),
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(s.maxAge)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			Subject:   "user",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(s.secretKey)
+
+	if err != nil {
+		log.Debug("jwt create error", slog.Any("err", err))
+		return nil, ErrCreateError
+	}
+
+	return []byte(tokenString), nil
+}
+
+func (s *SecretJWT) DeleteSecret(ctx context.Context, secret []byte) error {
+	log := s.logger.With(slog.String("method", "DeleteSecret"))
+
+	claims, err := s.decodeToken(secret)
+
+	if err != nil {
+		log.Debug("jwt parse error", slog.Any("err", err))
+		return ErrVerifyError
+	}
+	if s.jwtRevoker.IsJWTRevoked(ctx, claims.TokenId) {
+		return ErrVerifyError
+	}
+	s.jwtRevoker.RevokeJWT(ctx, claims.TokenId)
+	return nil
 }
