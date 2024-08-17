9f9214ea090993cc97a6f21c6dab427ca82aa02f
Галеев Ильдар <ildar-galeev92@mail.ru>
2024-08-17 20:15:08 +0700

    doc: fix godoc comment

--- internal/app/configapp/configApp.go
+++ internal/app/configapp/configApp.go
@@ -8,15 +8,15 @@ import (
 )
 
 type AppConfig struct {
-	EnvMode   string `env:"ENV_MODE" env-default:"prod"`
-	Port      int    `env:"PORT" env-default:"9090"`
-	Dsn       string `env:"DSN" env-require:"true"`
+	EnvMode string `env:"ENV_MODE" env-default:"prod"`
+	Port    int    `env:"PORT" env-default:"9090"`
+	Dsn     string `env:"DSN" env-require:"true"`
 
-	SecretKey []byte `env:"SECRET_KEY" env-require:"true"`
+	SecretKey     []byte        `env:"SECRET_KEY" env-require:"true"`
 	SecretsMaxAge time.Duration `env:"SECRETS_MAX_AGE" env-default:"24h"`
 }
 
-// Returns app configuration. Panic if failed
+// MustLoadConfig returns app configuration. Panic if failed
 func MustLoadConfig() *AppConfig {
 	//TODO: add loading config from file
 	var appConf AppConfig
