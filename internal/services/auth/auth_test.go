01d9f50a5ad7c8538d5554a5e21172571e8ab0f5
Галеев Ильдар <ildar-galeev92@mail.ru>
2024-08-21 00:45:57 +0700

    test: add tests for authservice.CreateUserSecret

--- internal/services/auth/auth_test.go
+++ internal/services/auth/auth_test.go
@@ -5,8 +5,12 @@ import (
 	"errors"
 	"github.com/IldarGaleev/todo-backend-service/internal/lib/secretsjwt/secretsdto"
 	"github.com/IldarGaleev/todo-backend-service/internal/services/auth/mocks"
+	"github.com/IldarGaleev/todo-backend-service/internal/services/servicedto"
+	"github.com/IldarGaleev/todo-backend-service/internal/storage"
+	storageDTO "github.com/IldarGaleev/todo-backend-service/internal/storage/models"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
 	"io"
 	"log/slog"
 	"testing"
@@ -72,4 +76,144 @@ func TestAuthService_CheckSecret_Invalid(t *testing.T) {
 
 	require.ErrorIs(t, err, ErrWrongSecret)
 	require.Nil(t, usr)
+}
+
+func prepareAccountGetter(ag *mocks.IAccountGetter, userPassword string, t *testing.T) {
+
+	pwdHash, err := bcrypt.GenerateFromPassword([]byte(userPassword), bcrypt.DefaultCost)
+
+	if err != nil {
+		t.Fail()
+		t.Logf("create hash error\n")
+	}
+
+	someUser := storageDTO.User{
+		PasswordHash: pwdHash,
+	}
+
+	ag.On(
+		"GetAccountByUsername",
+		mock.Anything,
+		mock.Anything,
+	).Return(&someUser, nil)
+}
+
+func TestAuthService_CreateUserSecret_Valid(t *testing.T) {
+	ctx := context.Background()
+	secretProvider, accountGetter, authService := createAuthService(t)
+
+	userPassword := "secret"
+	username := "test_user"
+	prepareAccountGetter(accountGetter, userPassword, t)
+
+	secretProvider.On(
+		"CreateSecret",
+		mock.Anything,
+		mock.Anything,
+	).Return([]byte("generated_token"), nil)
+
+	token, err := authService.CreateUserSecret(ctx, servicedto.User{Username: &username, Password: userPassword})
+
+	require.NoError(t, err)
+	require.Equal(t, "generated_token", token)
+}
+
+func TestAuthService_CreateUserSecret_CreateSecret_InternalError(t *testing.T) {
+	ctx := context.Background()
+	secretProvider, accountGetter, authService := createAuthService(t)
+
+	userPassword := "secret"
+	username := "test_user"
+	prepareAccountGetter(accountGetter, userPassword, t)
+
+	secretProvider.On(
+		"CreateSecret",
+		mock.Anything,
+		mock.Anything,
+	).Return(nil, errors.New("create secret internal error"))
+
+	_, err := authService.CreateUserSecret(ctx, servicedto.User{Username: &username, Password: userPassword})
+
+	require.ErrorIs(t, err, ErrInternal)
+}
+
+func TestAuthService_CreateUserSecret_ByUserID_NotFound(t *testing.T) {
+	ctx := context.Background()
+	_, accountGetter, authService := createAuthService(t)
+
+	userId := uint64(1)
+
+	accountGetter.On(
+		"GetAccountByID",
+		mock.Anything,
+		mock.Anything,
+	).Return(nil, storage.ErrNotFound)
+
+	_, err := authService.CreateUserSecret(ctx, servicedto.User{UserID: &userId, Password: "pwd"})
+
+	require.ErrorIs(t, err, ErrNotFound)
+}
+
+func TestAuthService_CreateUserSecret_ByUserID_AccountGetterInternalError(t *testing.T) {
+	ctx := context.Background()
+	_, accountGetter, authService := createAuthService(t)
+
+	userId := uint64(1)
+
+	accountGetter.On(
+		"GetAccountByID",
+		mock.Anything,
+		mock.Anything,
+	).Return(nil, errors.New("account getter internal error"))
+
+	_, err := authService.CreateUserSecret(ctx, servicedto.User{UserID: &userId, Password: "pwd"})
+
+	require.ErrorIs(t, err, ErrInternal)
+}
+
+func TestAuthService_CreateUserSecret_InvalidUserData(t *testing.T) {
+	ctx := context.Background()
+	_, _, authService := createAuthService(t)
+
+	_, err := authService.CreateUserSecret(ctx, servicedto.User{})
+
+	require.ErrorIs(t, err, ErrArguments)
+}
+
+func TestAuthService_CreateUserSecret_InvalidUserPassword(t *testing.T) {
+	ctx := context.Background()
+	_, accountGetter, authService := createAuthService(t)
+
+	prepareAccountGetter(accountGetter, "secret", t)
+
+	testCases := []struct {
+		name          string
+		password      string
+		expectedError error
+	}{
+		{
+			name:          "empty password",
+			password:      "",
+			expectedError: ErrArguments,
+		},
+		{
+			name:          "invalid password",
+			password:      "wrong_password",
+			expectedError: ErrWrongSecret,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			username := "user"
+			_, err := authService.CreateUserSecret(
+				ctx,
+				servicedto.User{
+					Username: &username,
+					Password: testCase.password,
+				})
+
+			require.ErrorIs(t, err, testCase.expectedError)
+		})
+	}
 }
